04f3cde185ccdea480be7c4ad01da199aa0d8827
Salman Niazi <salman@sics.se>
2016-05-24 14:24:25 +0200

    added input split size param

--- src/main/java/se/kth/MRMain.java
+++ src/main/java/se/kth/MRMain.java
@@ -10,6 +10,8 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
+
 
 public class MRMain extends Configured implements Tool {
 
@@ -22,29 +24,25 @@ public class MRMain extends Configured implements Tool {
 
   public int run(String[] args) throws Exception {
     Configuration conf = getConf();
-//    for(int i = 0 ; i < args.length; i++){
-//      String arg = args[i];
-//      if(arg.compareToIgnoreCase("-webhdfs") == 0){
-//       if((i+1) < args.length){
-//         conf.set("webhdfs", args[i+1]);
-//       }else{
-//         throw new IllegalArgumentException("specify webhdfs address");
-//       }
-//      }
-//    }
 
     Job job = new Job(conf, "HDFS-Stats");
-    if(conf.get("oivWebHdfs")!=null){
-      LOG.info("oivWebHdfs is set to "+conf.get("oivWebHdfs"));
-    } else {
-      LOG.info("oivWebHdfs is not set ");
-      for(String arg : args){
-        LOG.info("Param "+arg);
-      }
+    for(String arg : args){
+      LOG.info("Params "+arg);
+    }
+    String inputSplit= conf.get("splitSize");
+    if(inputSplit!=null){
+      LOG.info("InputSplit is set to "+inputSplit);
+      conf.set("mapreduce.input.fileinputformat.split.maxsize",inputSplit);
+    }
+
+    String webHdfs= conf.get("oivWebHdfs");
+    if(webHdfs!=null){
+      LOG.info("WebHdfs is set to "+webHdfs);
     }
     job.setJarByClass(this.getClass());
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.setInputFormatClass(CombineTextInputFormat.class);
     job.setMapperClass(Map.class);
     job.setCombinerClass(Reduce.class);
     job.setReducerClass(Reduce.class);
